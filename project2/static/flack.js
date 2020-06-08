36bdec97736f29b36f4ba9ae9ea1817b350ffb7f
Jay <jamesmac@Jamess-MacBook-Pro.local>
2020-06-08 03:55:11 +0100

    disabled cookie

--- project2/static/flack.js
+++ project2/static/flack.js
@@ -3,9 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	document.querySelector('#login').onsubmit = function() {
 
-		alert('you submitted');
-
 		const name = document.querySelector('#displayName').value;
+
+		alert(name);
+
 
 		localStorage.setItem('displayName', name);
 
