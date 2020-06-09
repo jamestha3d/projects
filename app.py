8d151d5c49fdd627b01dab6beb45fdbdc3135768
jamestha3d <jamesikediashi@gmail.com>
2020-06-09 03:03:10 +0100

    battling sockets

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from flask_session import Session
@@ -17,11 +18,26 @@ users = {}
 @app.route("/")
 def index():
 	#if not logged in
-	#redirect to log in
+	#redirect to log in		
 	return render_template("index.html", channels=channels)
 
-@app.route("/login")
+
+@app.route("/login", methods=["POST"])
 def login():
+	#save username and log in
+	name = request.form.get("displayName")
+	if not name:
+		return render_template("error.html", message="Invalid Display Name")
+
+	#check if display name is not currently in system
+	if name in users:
+		return render_template("error.html", message="Username Already Online")
+
+	return jsonify({"name": name })
+
+
+@app.route("/name")
+def name():
 	if request.method == "GET":
 		return render_template("login.html")
 	else:
@@ -47,10 +63,16 @@ def create():
 		return redirect(url_for('index', alert="Error! Channel Already exists!"))
 	channels.append(channel)
 	#return render_template("index.html", name=name, channels=channels)
-	return redirect(url_for('index', alert="Success!"))
+	return jsonify({"success": True, "channels": channels})
+	#return redirect(url_for('index', alert="Success!"))
 
 @app.route("/channel/<string:channel>")
 def channel(channel):
 	#
 	return "TO DO"
+
+@socketio.on("create channel")
+def create_channel(data):
+	channel = data["channel"]
+	emit("channel created", {"channel": channel}, broadcast=True)
 
--- static/flack.js
+++ static/flack.js
@@ -14,10 +14,32 @@ else{
 
 document.addEventListener('DOMContentLoaded', function () {
 
+
+	//connect to websocket
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+	
+	//when connecte, configure buttons
+	socket.on('connect', () => {
+
+		//submit "create channel"
+		document.querySelector('#create').onclick = () => {
+			const channel = document.querySelector('#channel').value;
+			socket.emit('create channel', {'channel': channel});
+		};
+	});
+
+
+	socket.on('channel created', data => {
+		const li = document.createElement('li');
+		li.innerHTML = `${data.channel}`;
+		document.querySelector('#channels').append(li);
+	});
+
 	document.querySelector('#login').onsubmit = function() {
 
 		const name = document.querySelector('#displayName').value;
 
+		
 		if (name === ''){
 
 			alert('Choose a Display Name');
@@ -30,14 +52,51 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 		
 
+
+		//HTTP request
+
+		//initialize new request
+		const request = new XMLHttpRequest();
+		request.open('POST', '/login');
+
+		//callback function for when request completes
+		request.onload = () => {
+
+			//Extract JSON data from request
+			const data = JSON.parse(request.responseText);
+
+		}
+
+		//add data to send with request.
+		const data = new FormData();
+		data.append('name', name)
+
+		//send request
+		request.send(data);
 		//return false;
 	}; 
 
+	const greet = localStorage.getItem('displayName');
+	document.querySelector('#greet').innerHTML = greet;
+
+
 	document.querySelector("#logout").onclick = function(){
 
-		localStorage.removeItem();
-		
+		localStorage.removeItem('displayName');
 
 	}
-	
+
+	function load(page){
+
+		const request = new XMLHttpRequest();
+		request.open('GET', `/${page}`);
+		request.onload = () => {
+			const response = request.responseText;
+			document.querySelector('#body').innerHTML = response;
+		};
+		request.send()
+
+	};
+
+
 });
